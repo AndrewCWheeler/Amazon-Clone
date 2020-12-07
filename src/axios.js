4e4613ffbad847b61433561d5c38c216ee9c38c7
AndrewCWheeler <achristopherwheeler@gmail.com>
2020-12-07 11:56:53 -0800

    Portfolio Build

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const instance = axios.create({
-  baseURL: 'http://localhost:5001/clone-c3595/us-central1/api'
+  baseURL: 'https://us-central1-clone-c3595.cloudfunctions.net/api'
 });
 
 export default instance;
